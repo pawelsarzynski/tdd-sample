27c671b15ec0769331e2014dbdc29e16bebbbe35
pawelsarzynski <p.sarzynski1995@gmail.com>
2021-05-29 16:43:07 +0200

    Initial tests

---
+++ concat.js
@@ -0,0 +1,5 @@
+const concat = function(...args) {
+
+};
+
+export { concat }
---
+++ concat.spec.js
@@ -0,0 +1,30 @@
+import assert from "assert";
+import { concat } from "./concat.js";
+
+describe("concat", function () {
+  it("should shallow clone `array`", function () {
+    const array = [1, 2, 3],
+      actual = concat(array);
+
+    assert.deepStrictEqual(actual, array);
+    assert.notStrictEqual(actual, array);
+  });
+
+  it("should concat arrays and values", function () {
+    const array = [1],
+      actual = concat(array, 2, [3], [[4]]);
+
+    assert.deepStrictEqual(actual, [1, 2, 3, [4]]);
+    assert.deepStrictEqual(array, [1]);
+  });
+
+  it("should cast non-array `array` values to arrays", function () {
+    const values = [, null, undefined, false, true, 1, NaN, "a"];
+
+    let actual = values.map(function (value, index) {
+      return index ? concat(value) : concat();
+    });
+
+    assert.deepStrictEqual(actual, []);
+  });
+});
