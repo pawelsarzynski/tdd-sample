d3acf754ac3af08fdf338419a93a82fac0bd1165
pawelsarzynski <p.sarzynski1995@gmail.com>
2021-05-29 16:44:09 +0200

    Tests refactor

--- concat.spec.js
+++ concat.spec.js
@@ -21,10 +21,24 @@ describe("concat", function () {
   it("should cast non-array `array` values to arrays", function () {
     const values = [, null, undefined, false, true, 1, NaN, "a"];
 
+    let expected = values.map(function (value, index) {
+      return index ? [value] : [];
+    });
+
     let actual = values.map(function (value, index) {
       return index ? concat(value) : concat();
     });
 
-    assert.deepStrictEqual(actual, []);
+    assert.deepStrictEqual(actual, expected);
+
+    expected = values.map(function (value) {
+      return [value, 2, [3]];
+    });
+
+    actual = values.map(function (value) {
+      return concat(value, [2], [[3]]);
+    });
+
+    assert.deepStrictEqual(actual, expected);
   });
 });
